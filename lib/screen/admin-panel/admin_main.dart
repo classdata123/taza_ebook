cb8c92b75615cf31a3e84b0a9bca3a38474be61d
TazaTeam <Classwork576@gmail.com>
2025-03-18 16:09:41 +0500

    drawer

--- lib/screen/admin-panel/admin_main.dart
+++ lib/screen/admin-panel/admin_main.dart
@@ -2,14 +2,22 @@ import 'package:ebookapp/screen/admin-panel/drawer.dart';
 import 'package:ebookapp/utility/app_content.dart';
 import 'package:flutter/material.dart';
 
-class admin extends StatelessWidget {
-  const admin({super.key});
+void main() {
+  runApp(MaterialApp(home: Admin()));
+}
+
+class Admin extends StatelessWidget {
+  const Admin({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: AppConstant.appMainbg),
-      drawer: draw(),
+      appBar: AppBar(backgroundColor: AppConstant.appMainColor),
+
+      drawer: Draw(), // Ensure this matches the class name in drawer.dart
+      body: Center(
+        child: Text("Admin Panel"),
+      ), // Added a body for better display
     );
   }
 }
