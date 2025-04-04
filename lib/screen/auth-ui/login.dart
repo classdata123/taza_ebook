a38d5794ee9fe344d4ef959834b7effb72ccc5ae
classdata123 <classdata321@gmail.com>
2025-04-04 20:52:29 +0500

    push

--- lib/screen/auth-ui/login.dart
+++ lib/screen/auth-ui/login.dart
@@ -32,10 +32,6 @@ class _LoginScreenState extends State<LoginScreen> {
           appBar: AppBar(
             backgroundColor: Colors.white,
             elevation: 0,
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: () => Get.back(),
-            ),
           ),
           body: Center(
             child: SingleChildScrollView(
